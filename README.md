68849fd0cc33fae3880cb1916c85cf7392139efb
iammasterbrucewayne <iammasterbrucewayne@protonmail.com>
2024-03-16 17:59:45 +0530

    add run_binary function

---
+++ README.md
@@ -0,0 +1,9 @@
+# Rust State Machine Tester
+
+This is a program that validates your progress on the Rust State Machine course.
+
+Usage:
+
+```
+./target/debug/rust_tester --stage=1 --binary-path=./subject_bin
+```
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,48 @@
+use std::process::{Command, Child};
+use std::time::Duration;
+use std::thread::sleep;
+use std::env;
+use std::process;
+
+const BINARY_PATH_FLAG: &str = "--binary-path=";
+
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome to the Rust State Machine course!\n");
+
+    let args: Vec<String> = env::args().collect();
+    let binary_path = args.iter().find(|arg| arg.starts_with(BINARY_PATH_FLAG));
+
+    let binary_path = match binary_path {
+        Some(path) => &path[BINARY_PATH_FLAG.len()..],
+        None => {
+            println!("The --binary-path flag must be specified");
+            process::exit(1);
+        }
+    };
+
+    println!("Binary Path = {}\n", binary_path);
+
+    let mut cmd = match run_binary(binary_path) {
+        Ok(cmd) => cmd,
+        Err(err) => {
+            println!("Error when starting process: {}", err);
+            process::exit(1);
+        }
+    };
+
+    sleep(Duration::from_secs(1));
+
+    match cmd.kill() {
+        Ok(_) => println!("Waiting for process to exit {}", binary_path),
+        Err(err) => println!("Error when killing process: {}", err),
+    }
+
+    match cmd.wait() {
+        Ok(_) => println!("Tests done"),
+        Err(err) => println!("Error when waiting for process: {}", err),
+    }
+}
+
+fn run_binary(binary_path: &str) -> std::io::Result<Child> {
+    Command::new(binary_path).spawn()
 }
