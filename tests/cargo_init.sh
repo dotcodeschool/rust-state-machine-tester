038e4d297f5311557977fe661ee62078ea73c803
iammasterbrucewayne <batman@dotcodeschool.com>
2024-10-21 21:53:03 +0530

    fix(tests/cargo_init.sh): fix script for relaying program output

--- tests/cargo_init.sh
+++ tests/cargo_init.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 output=$(cargo run)
-if [ $? -ne 0 ]; then
-echo "Test Failed: The program did not exit successfully."
-exit 1
-elif [[ $output == *"Hello, world!"* ]] && [[ $output != *"panic"* ]]; then
-echo "Congrats! You've successfully initialized your Rust Project."
-else
-echo "Looks like `cargo run` is failing to execute. Maybe there's a bug in your code?"
-exit 1
+status=$?
+
+echo $output
+
+if [ $status -ne 0 ]; then    
+    echo "Error: command \`cargo run\` exited with code $status"
+    exit $status
 fi
