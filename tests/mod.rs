1ad5e058522f7c26f6c340b221e5b506dba27ca0
iammasterbrucewayne <batman@dotcodeschool.com>
2024-10-23 21:38:06 +0530

    fix: update test paths path

---
+++ tests/mod.rs
@@ -0,0 +1,8 @@
+#[path = "2-1/test_pallet_struct_exists.rs"]
+mod test_pallet_struct_exists;
+
+#[path = "2-2/test_balances_pallet_implementation.rs"]
+mod test_balances_pallet_implementation;
+
+#[path = "2-3/test_balances_interactions.rs"]
+mod test_balances_interactions;
