ae9a514f521c1623eb2b3d27476f4829311c85ac
iammasterbrucewayne <batman@dotcodeschool.com>
2024-11-03 10:15:09 +0530

    fix(tests/3-2): update 3-2 to allow configurable balances

--- tests/3-2/system_interactions_wrapper.sh
+++ tests/3-2/system_interactions_wrapper.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 ./tests/helpers/update_version.sh "0.3.2"
 
+BALANCES_FILE="src/balances.rs"
+
 source ./tests/helpers/check_struct_and_update_version.sh
-check_struct_and_update_version "src/balances.rs" "pub struct Pallet<AccountId, Balance>" "0.4.3"
+
+check_struct_and_update_version "$BALANCES_FILE" "pub struct Pallet<AccountId, Balance>" "0.4.3"
+check_struct_and_update_version "$BALANCES_FILE" "pub struct Pallet<T: Config>" "0.4.6"
 
 cargo test test_system_interactions
